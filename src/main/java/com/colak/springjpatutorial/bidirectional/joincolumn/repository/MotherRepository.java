f453dcd42e24193a8b2680ee54d244d459da3a45
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-19 09:27:21 +0300

    example for custom method

--- src/main/java/com/colak/springjpatutorial/bidirectional/joincolumn/repository/MotherRepository.java
+++ src/main/java/com/colak/springjpatutorial/bidirectional/joincolumn/repository/MotherRepository.java
@@ -3,5 +3,10 @@ package com.colak.springjpatutorial.bidirectional.joincolumn.repository;
 import com.colak.springjpatutorial.bidirectional.joincolumn.jpa.Mother;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MotherRepository extends JpaRepository<Mother, Integer> {
+
+    // Custom method to get Mother by Child id
+    Optional<Mother> findByChildId(Long productId);
 }
---
+++ src/test/java/com/colak/springjpatutorial/bidirectional/joincolumn/repository/MotherRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.colak.springjpatutorial.bidirectional.joincolumn.repository;
+
+import com.colak.springjpatutorial.bidirectional.joincolumn.jpa.Child;
+import com.colak.springjpatutorial.bidirectional.joincolumn.jpa.Mother;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MotherRepositoryTest {
+
+    @Autowired
+    private MotherRepository repository;
+
+    @Test
+    void findByChildId() {
+        // Child is saved first, then mother
+        Mother mother = new Mother();
+
+        Child child = new Child();
+
+        child.setMother(mother);
+        mother.setChild(child);
+        repository.save(mother);
+
+        Optional<Mother> byChildId = repository.findByChildId(1L);
+        assertThat(byChildId).isPresent();
+    }
+}
